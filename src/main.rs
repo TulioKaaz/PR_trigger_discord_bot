0ec2d9acf5f57e262357e117e5884a15b6ccbc24
TulioKaaz <tuliocaz.windcoast@gmail.com>
2024-04-09 09:52:14 -0300

    test

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,39 @@
-use octocrab::{Octocrab, Result};
+use octocrab::{etag::Etagged, models::events::Event, Octocrab, Page, Result};
+use std::{thread, time, env};
+use dotenv::dotenv;
 
 struct RepoData {
     owner: String,
     repo: String,
 }
 
-async fn monitor_pull_requests(repo_data: RepoData, github_token: str) -> Result<()> {
+async fn monitor_pull_requests(repo_data: RepoData, github_token: String) -> Result<Etagged<Page<Event>>> {
+    let delay = time::Duration::from_secs(10);
     let octocrab = Octocrab::builder().personal_token(github_token).build()?;
-
-    let mut stream = octocrab.repos(owner, repo).events().per_page(100).send().await?;
-
-    stream
+    let mut etag = None;
+    loop {
+        let response = octocrab.repos(&repo_data.owner, &repo_data.repo).events().etag(etag).send().await?;
+        if let Some(page) = response.value {
+            for event in page.items {
+                println!("{:?}", event);
+            }
+        } else {
+            println!("No New Data Received");
+        }
+        etag = response.etag;
+        thread::sleep(delay);
+    }
 }
 
-fn main() {
-    monitor_pull_requests(RepoData {
-        owner: "TulioKaaz",
-        repo: "octocrab",
-    }, "ghp_...").await;
-    println!("{}", );
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+    let repo_data = RepoData {
+        owner: "TulioKaaz".to_string(),
+        repo: "PR_trigger_discord_bot".to_string(),
+    };
+    let github_token = env::var("GITHUB_TOKEN").expect("GITHUB_TOKEN is not set in .env file");
+
+
+    monitor_pull_requests(repo_data, github_token).await.unwrap();
 }
